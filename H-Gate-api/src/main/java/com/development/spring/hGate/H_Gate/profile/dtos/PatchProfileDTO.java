7e188a10aa299c5a852b26e12ac125aa54602293
rinotesta <pellegrino.testa@teoresigroup.com>
2025-12-09 13:06:30 +0100

    modifiche dto per cambio password

--- H-Gate-api/src/main/java/com/development/spring/hGate/H_Gate/profile/dtos/PatchProfileDTO.java
+++ H-Gate-api/src/main/java/com/development/spring/hGate/H_Gate/profile/dtos/PatchProfileDTO.java
@@ -1,12 +1,11 @@
 package com.development.spring.hGate.H_Gate.profile.dtos;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import lombok.*;
 
 @Data
+@Setter
+@Getter
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
@@ -23,43 +22,4 @@ public class PatchProfileDTO {
 
     private String newPassword;
 
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getSurname() {
-        return surname;
-    }
-
-    public void setSurname(String surname) {
-        this.surname = surname;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    public String getNewPassword() {
-        return newPassword;
-    }
-
-    public void setNewPassword(String newPassword) {
-        this.newPassword = newPassword;
-    }
 }
